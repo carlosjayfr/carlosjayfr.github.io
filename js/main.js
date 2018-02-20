b1b285b089de0576ce51913459dc3df513a13c96
carlosjayfr <carlosjfr2@hotmail.com>
2018-02-20 14:24:39 +0100

    Cambios con jQuery

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    //Menu hamburguesa
+    $(".burger").click(function(){
+        $(".menu").animate({width:"toggle"},500);
+    });
+    $(".cerrar").click(function(){
+        $(".menu").animate({width:"toggle"},500); 
+    });
+
+    //Menu hamburguesa páginas normales
+    $(".burger2").click(function(){
+        $(".burger2").toggleClass("abierto");
+        $(".menu2").animate({width:"toggle"},500);
+        $(".menu2 a").toggleClass("aparicion");
+    });
+
+    //Scroll suave
+    $("a").on("click", function(){
+        if (this.hash !== "") {
+            var hash= this.hash;
+            $("html, body").animate({
+                //Objeto con propiedad y valor
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    })
+
+    //Ventana Modal
+    $("#open").click(function(){
+        $(".overlay, .modal").addClass("visible");
+    });
+    $("#close, .overlay").click(function(){
+        $(".overlay, .modal").removeClass("visible");
+    });
+        
+
+});
